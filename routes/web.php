32d6cee02f680b35244072047109c812217253a0
Jerome Paulos <jeromepaulos@icloud.com>
2023-05-30 23:16:22 -0700

    Update README and add redirect

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', 'https://github.com/75grand/api', 301);
+
 Route::get('/auth/redirect', [AuthController::class, 'redirect'])->name('auth.redirect');
 Route::get('/auth/callback', [AuthController::class, 'callback'])->name('auth.callback');
