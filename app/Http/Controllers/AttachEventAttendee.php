ff9140559ef0ff14c7dc48e0eac87e46cf3d5cf1
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-06 23:42:17 -0700

    Split CalendarEventController into single action controllers

---
+++ app/Http/Controllers/AttachEventAttendee.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CalendarEvent;
+use Illuminate\Http\Request;
+
+class AttachEventAttendee extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(CalendarEvent $event, Request $request)
+    {
+        $data = $request->validate(['attending' => 'required|boolean']);
+        
+        if($data['attending']) {
+            $request->user()->events()->syncWithoutDetaching($event);
+        } else {
+            $request->user()->events()->detach($event);
+        }
+    }
+}
