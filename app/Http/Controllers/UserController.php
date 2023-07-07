9eba16d3151a223289662b6c60bb1b78bb623333
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-06 23:42:24 -0700

    Use $request instead of helper

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function show()
+    public function show(Request $request)
     {
-        return auth()->user();
+        return $request->user();
     }
 }
