2b7294802840abfc8e97c68b82b8332b70021705
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-06 23:38:10 -0700

    Rename FeedbackController

--- app/Http/Controllers/SendFeedback.php
+++ app/Http/Controllers/SendFeedback.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FeedbackController extends Controller
+class SendFeedback extends Controller
 {
     /**
      * Handle the incoming request.
@@ -16,9 +16,11 @@ class FeedbackController extends Controller
             'message' => 'required|string'
         ]);
 
-        webhook_alert('New feedback', [
-            'Email' => $data['email'] ?? '(no email provided)',
-            'Message' => $data['message']
-        ]);
+        dispatch(function() use ($data) {
+            webhook_alert('New feedback', [
+                'Email' => $data['email'] ?? '(no email provided)',
+                'Message' => $data['message']
+            ]);
+        })->afterResponse();
     }
 }
