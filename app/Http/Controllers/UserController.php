c78b72fe48aefb6f1ea6255937bb1fe7751fc49a
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-10 23:42:57 -0700

    Add validation for class year

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -13,10 +14,12 @@ class UserController extends Controller
 
     public function update(Request $request)
     {
+        $years = array_map(fn($n) => $n + date('Y'), range(0, 4));
+
         $data = $request->validate([
             'expo_token' => ['nullable', 'string'],
             'macpass_number' => ['nullable', 'string', 'numeric', 'digits:9'],
-            'class_year' => ['nullable', 'integer', 'digits:4'],
+            'class_year' => ['nullable', 'integer', Rule::in($years)],
             'position' => ['nullable', 'string', 'in:student,professor,staff'],
             'po_combination' => ['nullable', 'string', 'regex:/^\d+-\d+-\d+$/'],
             'po_number' => ['nullable', 'integer']
