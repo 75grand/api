6a335a491d53d8c4515f5635b95aa43fc3750025
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-15 13:49:06 -0700

    Add casts

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -9,6 +9,14 @@ class Professor extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'ratings_distribution' => 'array',
+        'difficulty' => 'float',
+        'rating' => 'float',
+        'rating_count' => 'integer',
+        'take_again_percent' => 'integer'
+    ];
+
     public function courses()
     {
         return $this->hasMany(Course::class);
