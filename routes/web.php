593d492fcf93a8ea366e4005d396487e5fbbc9ac
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-20 14:03:08 -0700

    Quick action redirects

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', 'https://github.com/75grand/api', 301);
 
 Route::get('/auth/callback', [MobileAuthController::class, 'callback'])->name('auth.callback');
+
+Route::permanentRedirect('/redirect/reserve', 'https://ems.macalester.edu');
+Route::permanentRedirect('/redirect/library-catalog', 'https://macalester.on.worldcat.org/discovery');
+Route::permanentRedirect('/redirect/print', 'https://macalester.us.uniflowonline.com/#StartPrinting');
+Route::permanentRedirect('/redirect/moodle', 'https://moodle.macalester.edu/login/index.php?authCAS=CAS');
+Route::permanentRedirect('/redirect/time-clock', 'https://cas.tcplusondemand.com/43341/App_Redirect/webclock.aspx');
