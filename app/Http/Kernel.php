0d9e22e6add5ef529bc3bb51bb11f6a41da2e99d
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-26 00:37:36 -0700

    Authorize sanctum users for unguarded routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'guard:sanctum',
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -63,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'guard' => \App\Http\Middleware\SwitchGuard::class,
     ];
 }
---
+++ app/Http/Middleware/SwitchGuard.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @see https://dannyherran.com/2022/01/laravel-sanctum-optional-route-authentication-guard/
+ */
+class SwitchGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $guard = null): Response
+    {
+        if(in_array($guard, array_keys(config('auth.guards')))) {
+            config(['auth.defaults.guard' => $guard]);
+        }
+
+        return $next($request);
+    }
+}
