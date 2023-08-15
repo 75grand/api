fd8ac6aecfbea320d2ae189a4b1b93659c7e0dea
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-15 02:12:12 -0700

    Fix route-model binding for attendees

--- app/Http/Controllers/EventAttendeeController.php
+++ app/Http/Controllers/EventAttendeeController.php
@@ -10,24 +10,24 @@ class EventAttendeeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(CalendarEvent $calendarEvent)
+    public function index(CalendarEvent $event)
     {
-        return $calendarEvent->users()->select(['id', 'avatar'])->get();
+        return $event->users()->select(['id', 'avatar'])->get();
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, CalendarEvent $calendarEvent)
+    public function update(CalendarEvent $event, Request $request)
     {
         $data = $request->validate(['attending' => 'required|boolean']);
         
         if($data['attending']) {
-            $request->user()->events()->syncWithoutDetaching($calendarEvent);
+            $request->user()->events()->syncWithoutDetaching($event);
         } else {
-            $request->user()->events()->detach($calendarEvent);
+            $request->user()->events()->detach($event);
         }
 
-        return $this->index($calendarEvent);
+        return $this->index($event);
     }
 }
