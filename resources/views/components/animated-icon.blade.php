c2ae48d9982c475a8178a1ffb20477871777c593
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-23 10:47:09 -0700

    Change times to 9:41

--- resources/views/components/animated-icon.blade.php
+++ resources/views/components/animated-icon.blade.php
@@ -3,10 +3,9 @@
 <div class="relative">
     <img class="absolute scale-[67%]" src="{{ url('/assets/icon.svg') }}" alt="">
 
-    <svg class="animate-spin [animation-duration:15s]" viewBox="0 0 {{ $scale }} {{ $scale }}">
+    <svg class="animate-spin [animation-duration:20s] [animation-delay:-5s]" viewBox="0 0 {{ $scale }} {{ $scale }}">
         <defs>
-            <circle id="circle" r="{{ $scale * 0.37 }}"
-                cx="{{ $scale / 2 }}" cy="{{ $scale / 2 }}"/>
+            <circle id="circle" r="{{ $scale * 0.37 }}" cx="{{ $scale / 2 }}" cy="{{ $scale / 2 }}"/>
         </defs>
     
         <text>
