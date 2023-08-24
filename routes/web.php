2b987d062e5324060a6770fba3a2eaeaaed2d429
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-23 23:07:26 -0700

    Add app store redirects

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,16 @@ Route::view('/', 'home')->name('home');
 Route::view('/privacy', 'privacy', headers: ['Content-Type' => 'text/plain'])->name('privacy');
 Route::view('/terms', 'terms', headers: ['Content-Type' => 'text/plain'])->name('terms');
 
+Route::redirect('/download/ios', 'https://apple.co/45DznpV')->name('download.ios');
+Route::redirect('/download/android', 'https://play.google.com/store/apps/details?id=zone.jero.grand')->name('download.android');
+
 Route::get('/auth/callback', [MobileAuthController::class, 'callback'])->name('auth.callback');
 
 Route::get('/auth/login', [AdminAuthController::class, 'redirect'])->name('login');
 Route::get('/auth/browser-callback', [AdminAuthController::class, 'callback'])->name('auth.browser.callback');
 
-Route::permanentRedirect('/redirect/reserve', 'https://ems.macalester.edu');
-Route::permanentRedirect('/redirect/library-catalog', 'https://macalester.on.worldcat.org/discovery');
-Route::permanentRedirect('/redirect/print', 'https://macalester.us.uniflowonline.com/#StartPrinting');
-Route::permanentRedirect('/redirect/moodle', 'https://moodle.macalester.edu/login/index.php?authCAS=CAS');
-Route::permanentRedirect('/redirect/time-clock', 'https://cas.tcplusondemand.com/43341/App_Redirect/webclock.aspx');
+Route::redirect('/redirect/reserve', 'https://ems.macalester.edu');
+Route::redirect('/redirect/library-catalog', 'https://macalester.on.worldcat.org/discovery');
+Route::redirect('/redirect/print', 'https://macalester.us.uniflowonline.com/#StartPrinting');
+Route::redirect('/redirect/moodle', 'https://moodle.macalester.edu/login/index.php?authCAS=CAS');
+Route::redirect('/redirect/time-clock', 'https://cas.tcplusondemand.com/43341/App_Redirect/webclock.aspx');
