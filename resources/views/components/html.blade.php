776b0e69b41ad9a36f43e8bd4ecec472d8f98119
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-23 23:07:32 -0700

    Add favicon

--- resources/views/components/html.blade.php
+++ resources/views/components/html.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="icon" href="{{ url('/assets/icon.svg') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap">
     <title>{{ $title }}</title>
     @vite('resources/css/app.css')
