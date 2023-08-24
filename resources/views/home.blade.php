c12b670f1897395643ae1ced6c35790012f88432
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-23 23:02:15 -0700

    Use random slogan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,18 @@
+@php
+    $slogans = [
+        'The Macalester App',
+        'Your Portal to Macalester',
+        'Macalester & More',
+        'More Than Macalester'
+    ];
+
+    $slogan = $slogans[array_rand($slogans)];
+@endphp
+
 <x-html title="75grand: The Macalester App">
     <div class="p-16 flex flex-col items-center text-center gap-16">
         <div class="w-40">
-            <x-animated-icon text="Your Portal to Macalester"/>
+            <x-animated-icon text="{!! $slogan !!}"/>
         </div>
 
         <div class="space-y-4">
@@ -16,6 +27,7 @@
                 MacPass,
                 P.O. box combination
             </h2>
+            <h1 class="text-4xl font-semibold">{{ $slogan }}</h1>
         </div>
 
         <div class="flex gap-4">
