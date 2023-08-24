5f4ac93f00069ef5e09b857a537fb77b60f27bfe
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-24 01:57:25 -0700

    Add mailing list

---
+++ resources/views/components/email-form.blade.php
@@ -0,0 +1,17 @@
+<div class="space-y-4">
+    <p class="text-gray-500">Coming soon to iOS and Android!</p>
+
+    <form method="POST" action="https://app.us21.list-manage.com/subscribe/post" target="_blank">
+        <input type="hidden" name="u" value="d47e526a51c281ea13ce13ff4">
+        <input type="hidden" name="id" value="f699b0c2f7">
+    
+        <div class="flex gap-2">
+            <input class="border-2 px-4 py-2 rounded-xl outline-none focus-within:border-accent transition-colors"
+                type="email" name="EMAIL" placeholder="mscot@macalester.edu" required
+                    autocapitalize="none" autocorrect="off">
+        
+            <button class="bg-accent text-white border-2 border-accent rounded-xl px-4 py-2 font-semibold
+                active:scale-90 transition-transform" type="submit">Notify</button>
+        </div>
+    </form>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,25 @@
-@php
-    $slogans = [
-        'The Macalester App',
-        'Your Portal to Macalester',
-        'Macalester & More',
-        'More Than Macalester'
-    ];
-
-    $slogan = $slogans[array_rand($slogans)];
-@endphp
+{{--
+    More Than Macalester
+    Macalester & More
+    The Macalester App
+    Your Portal to Macalester
+--}}
 
 <x-html title="75grand: The Macalester App">
     <div class="sm:p-16 p-8 flex flex-col items-center text-center gap-16">
         <div class="w-40">
-            <x-animated-icon text="{!! $slogan !!}"/>
+            <x-animated-icon text="Your Portal to Macalester"/>
         </div>
 
         <div class="space-y-4">
-            <h1 class="text-4xl font-semibold">{{ $slogan }}</h1>
+            <h1 class="text-4xl font-semibold">The Macalester App</h1>
             <x-animated-subtitle/>
         </div>
 
         <div class="flex gap-4">
-            <x-app-store-badge platform="ios"/>
-            <x-app-store-badge platform="android"/>
+            <x-email-form/>
+            {{-- <x-app-store-badge platform="ios"/> --}}
+            {{-- <x-app-store-badge platform="android"/> --}}
         </div>
 
         <div class="max-w-md">
