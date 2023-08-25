0084657e455bc2f3afbe1377bad85718064c5daf
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-25 15:37:57 -0700

    Update delete account page

--- resources/views/delete-account.blade.php
+++ resources/views/delete-account.blade.php
@@ -1,5 +1,5 @@
 <x-page title="Delete Your Account">
 
-If you would like to delete your account or any data from your account, please email [support@75grand.net](mailto:support@75grand.net) and we will handle your request.
+If you would like to delete your account, please email [support@75grand.net](mailto:support@75grand.net) from your @macalester.edu address.
 
 </x-page>
