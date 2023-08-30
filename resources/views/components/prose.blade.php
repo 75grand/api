3cadbfd28ba8ac185c82403ca9b7d266bc66620e
Jerome Paulos <jeromepaulos@icloud.com>
2023-08-30 15:43:05 -0700

    Make prose text black

--- resources/views/components/prose.blade.php
+++ resources/views/components/prose.blade.php
@@ -1,5 +1,5 @@
 <div class="
-    prose prose-zinc
+    prose prose-zinc prose-p:text-black
     prose-a:text-accent prose-a:font-semibold prose-a:no-underline hover:prose-a:underline
 ">
     {{ $slot }}
