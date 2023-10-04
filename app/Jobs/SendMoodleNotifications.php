94940f4a7f55993d2fe260fba76d5459070b6471
Jerome Paulos <jeromepaulos@icloud.com>
2023-10-04 15:37:48 -0500

    what is going on

--- app/Jobs/SendMoodleNotifications.php
+++ app/Jobs/SendMoodleNotifications.php
@@ -12,7 +12,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Str;
 
-class SendDailyMoodleNotifications implements ShouldQueue
+class SendMoodleNotifications implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
