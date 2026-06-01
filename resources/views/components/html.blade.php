5c5372cb3c9a2557b16df5fc23b34b445553a1cf
Jerome Paulos <64276359+jxxe@users.noreply.github.com>
2026-06-01 12:18:09 -0700

    Replace Beam Analytics with OneDollarStats

--- resources/views/components/html.blade.php
+++ resources/views/components/html.blade.php
@@ -7,7 +7,7 @@
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap">
     @vite('resources/css/app.css')
-    <script async src="https://beamanalytics.b-cdn.net/beam.min.js" data-token="dc450d08-0ea1-4a44-8ba3-ef4139b189a1"></script>
+    <script defer src="https://assets.onedollarstats.com/stonks.js"></script>
 
     <title>{{ $title }}</title>
 
