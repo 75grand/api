d4841fa7cc2f3bbb201b779069e4623738a1e9e3
Jerome Paulos <64276359+jxxe@users.noreply.github.com>
2026-06-01 12:43:23 -0700

    Fix OneDollarStats link to specific policy page

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -5,7 +5,7 @@ _Last updated November 7, 2023._
 75grand uses third-party services to improve the user experience. Please review these services’ terms and conditions:
 
 - [Google Play Services](https://policies.google.com/terms) (on Android only)
-- [OneDollarStats](https://onedollarstats.com) (on the website only)
+- [OneDollarStats](https://onedollarstats.com/terms) (on the website only)
 - [Expo](https://expo.dev/terms)
 - [Sentry](https://sentry.io/terms/)
 - [June](https://help.june.so/en/articles/6823511-terms-of-service)
